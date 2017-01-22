a340b0fc0863bef904c2b19b13dd45ef8efdb46c
Jonathon Lantsberger <jtl378@mail.missouri.edu>
2017-01-22 15:09:09 -0600

    adding jtl378 sentences

--- story.c
+++ story.c
@@ -5,64 +5,66 @@
 
 int main( void )
 {
-	printf("\n");
-	bensSentence1();
-	printf("\n\n");
+    printf("\n");
+    bensSentence1();
+    printf("\n\n");
 
-	// Add your sentence.
-	// Don't forget to update the header file.
-	// vvvvvvvvvvvvvvvv STUDENT WORK HERE
+    // Add your sentence.
+    // Don't forget to update the header file.
+    // vvvvvvvvvvvvvvvv STUDENT WORK HERE
 
-	snhqm9_sentence1();
-	shensSentence1();
-	cgj6hb_sentence1();
-	mdsf3fSentence1();
-	pje279Sentence1();
-	rml3mdFirstSentence();
-	nicolasSmithsSentence1();
-	acmyfFirst();
-	jarpqdSentence1();
-	printf("\n\n");
-	jmpcz9NewSentence1();
-	yzghf();
-	jdckw4_sentence1();
-	printf("\n\n");
-	szz63one();
-	hagd5dSentence1();
-	sskgz8sentence1();
-        jah3xc_sentence1();
-	klft2();
-	mkc42Sentence1();
-	jdckw4_sentence2();
-	jarpqdSentence2();
-	scfpt6Sentence1();
+    snhqm9_sentence1();
+    jtl378_sentence1();
+    jtl378_sentence2();
+    shensSentence1();
+    cgj6hb_sentence1();
+    mdsf3fSentence1();
+    pje279Sentence1();
+    rml3mdFirstSentence();
+    nicolasSmithsSentence1();
+    acmyfFirst();
+    jarpqdSentence1();
+    printf("\n\n");
+    jmpcz9NewSentence1();
+    yzghf();
+    jdckw4_sentence1();
+    printf("\n\n");
+    szz63one();
+    hagd5dSentence1();
+    sskgz8sentence1();
+    jah3xc_sentence1();
+    klft2();
+    mkc42Sentence1();
+    jdckw4_sentence2();
+    jarpqdSentence2();
+    scfpt6Sentence1();
     jg229Sentence1();
-	printf("\n\n");
-	snhqm9_sentence2();
-	rml3mdSecondSentence();
-	cgj6hb_sentence2();
-	pje279Sentence2();
-	nicolasSmithsSentence2();
-	mdsf3fSentence2();
-        jah3xc_sentence2();
-	casty8Sentence1();
-	shensSentence_FINAL();
-	acmyfSecond();
-	jmpcz9NewSentence2();
-	yzghf2();
-	sskgz8sentence_FINAL();
-	szz63two();
-	hagd5dSentence2();
-	klft2_1();
+    printf("\n\n");
+    snhqm9_sentence2();
+    rml3mdSecondSentence();
+    cgj6hb_sentence2();
+    pje279Sentence2();
+    nicolasSmithsSentence2();
+    mdsf3fSentence2();
+    jah3xc_sentence2();
+    casty8Sentence1();
+    shensSentence_FINAL();
+    acmyfSecond();
+    jmpcz9NewSentence2();
+    yzghf2();
+    sskgz8sentence_FINAL();
+    szz63two();
+    hagd5dSentence2();
+    klft2_1();
     jg229Sentence2();
-	casty8Sentence2();
-	mkc42Sentence2();
-	scfpt6Sentence2();
+    casty8Sentence2();
+    mkc42Sentence2();
+    scfpt6Sentence2();
 
 
 
-	// DO NOT ADD AFTER THIS COMMENT
-	// Bens sentence should be last.
-	bensSentence_FINAL();
-	printf("\n\n");
+    // DO NOT ADD AFTER THIS COMMENT
+    // Bens sentence should be last.
+    bensSentence_FINAL();
+    printf("\n\n");
 }
