0916a4ed27f2bd59c7e12b88bece492e598485c5
Jeff Ruffolo <jarpqd@mail.missouri.edu>
2017-01-22 12:41:05 -0600

    Jarpqd files

---
+++ sentences/jarpqd.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+void jarpqdSentence1(){
+	printf("But, little did he know...");
+}
+
+void jarpqdSentence2(){
+	printf("He tried to take the blue pill, but there was a mix-up.  ");
+}
