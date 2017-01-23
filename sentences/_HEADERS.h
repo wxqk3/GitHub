8d39afcb2cf69cbe7ee79807a1334b6b21391c41
Yongfang0123 <yqzn9@mail.missouri.edu>
2017-01-23 01:07:24 -0600

    aa

--- sentences/_HEADERS.h
+++ sentences/_HEADERS.h
@@ -6,7 +6,8 @@ int bensSentence_FINAL();
 // DECLARE YOUR symbols here, implement them in a standalone C file
 // vvvvvvvvvvvvvvv  Student work goes down here
 
-
+int yqzn9_one();
+int yqzn9_two();
 int mtrmt9First();
 int mtrmt9Second();
 int nicksen1();
---
+++ sentences/yqzn9.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+
+// Function to include & executeA
+int yqzn9_one()
+{
+  printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+  return 1;
+}
+
+int yqzn9_two()
+{
+  printf("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+  return 1;
+}
