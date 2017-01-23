21f5259b7c840490fb7a84f565f6adc37b00714d
Alex_Buhse <alexbuhse1@gmail.com>
2017-01-23 16:14:27 -0600

    Updating makefile, story.c, and the_story.txt

--- story.c
+++ story.c
@@ -36,7 +36,6 @@ int main( void )
     jmpcz9NewSentence1();
 	kfjmbb_sentence2();
 	xwq58First();
-	ddpfc2Sentences1();
     printf("\n\n");
     jmpcz9NewSentence1();
     kjf3mdSentence1();
@@ -67,7 +66,6 @@ int main( void )
     cgj6hb_sentence2();
     pje279Sentence2();
     nicolasSmithsSentence2();
-	ddpfc2Senetence2();
     mdsf3fSentence2();
     jah3xc_sentence2();
     casty8Sentence1();
@@ -126,11 +124,8 @@ int main( void )
     aupkbdsentence2();
     xwq58Second();
     twv8f9Sentence2();
-<<<<<<< HEAD
-=======
 	asbrm5Sentence1();
 	asbrm5Sentence2();
->>>>>>> asbrm5
     // DO NOT ADD AFTER THIS COMMENT
     // Bens sentence should be last.
     bensSentence_FINAL();
