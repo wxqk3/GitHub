8df62485145ffd63e0e9977282d5c4f2f6523854
cjrz74 <cjrz74@mail.missouri.edu>
2017-01-23 20:26:00 -0600

    added sentences

--- sentences/cjrz74.c
+++ sentences/cjrz74.c
@@ -4,12 +4,12 @@
 // Function to include & execute
 int cjrz74Sentence1()
 {
-  printf(" Im a little late to the party. ");
+  printf(" Just at that moment it just accurred to Beethoven that it was 10 PM. The party had already started. ");
   return 1;
 }
 
 int cjrz74Sentence2()
 {
-  printf(" My sentence forming skills are below par. ");
+  printf(" Beethoven raced off at that moment to his destination to bring the real party. ");
   return 1;
 }
