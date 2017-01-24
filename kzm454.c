b776626fe019d0f64c821ab96ce4cddffe48a986
kezipper <zipperer@gmail.com>
2017-01-24 01:04:41 -0600

    Add files via upload

---
+++ kzm454.c
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+# include <stdio.h>
+
+// Functions to print sentences in story
+int kzm454Sentence1()
+{
+  printf("Who said that?");
+  return 1;
+}
+
+int kzm454Sentence2()
+{
+  printf("Chicken and waffles are my favorite.");
+  return 1;
+}
+
